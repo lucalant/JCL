d19d4c6e3a5ae3ce1694f52e0a048194fb8bbdb4
lucalant <luca@lant.it>
2013-06-18 17:58:18 +0200

    gitignore con commentati idb sql

---
+++ db/schema.rb
@@ -0,0 +1,83 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20130618151719) do
+
+  create_table "designers", force: true do |t|
+    t.string   "name"
+    t.string   "surname"
+    t.text     "description"
+    t.string   "img_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "distributions", force: true do |t|
+    t.string   "presentation"
+    t.string   "img_url"
+    t.string   "map"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "events", force: true do |t|
+    t.string   "about"
+    t.text     "video"
+    t.string   "img_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "home_products", force: true do |t|
+    t.string   "images"
+    t.string   "text"
+    t.string   "introduction"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "partners", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "img_url"
+    t.string   "contact"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "services", force: true do |t|
+    t.string   "description"
+    t.string   "img_url"
+    t.string   "type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "top_c_lients", force: true do |t|
+    t.string   "name"
+    t.string   "surname"
+    t.string   "img_url"
+    t.string   "about"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "workspaces", force: true do |t|
+    t.string   "presentation"
+    t.string   "img_url"
+    t.string   "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
