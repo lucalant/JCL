7d304c022a90daee926c29ff7c31d3a5143ee16b
lucalant <luca@lant.it>
2013-06-20 16:23:04 +0200

    Tutti i controller e tutte le immagini

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,31 @@
 class AdminController < ApplicationController
-
   def index
-
   end
 
+  def designer
+  end
+
+  def distribution
+  end
+
+  def event
+  end
+
+  def homeProduct
+  end
+
+  def partner
+  end
+
+  def service
+  end
+
+  def topClient
+  end
+
+  def workspace
+  end
+
+  def company
+  end
 end
