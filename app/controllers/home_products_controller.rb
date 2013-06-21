46d7bee9e32aecb88a4e9f98c1d491b8ad932cc3
lucalant <luca@lant.it>
2013-06-21 15:06:09 +0200

    aggiunta pagine home product

---
+++ app/controllers/home_products_controller.rb
@@ -0,0 +1,28 @@
+class HomeProductsController < ApplicationController
+  def create
+  end
+
+  def products_by_room
+  end
+
+  def home_products_by_type
+  end
+
+  def new_home_products
+  end
+
+  def top_home_products
+  end
+
+  def index
+  end
+
+  def all_rooms
+  end
+
+  def all_types
+  end
+
+  def home_distribution
+  end
+end
