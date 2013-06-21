76e13ca76049cfeb2a92edcf69515f0c575efa34
juliamalovi <juliamalovi@gmail.com>
2013-06-21 16:15:24 +0200

    added tuple in the designer table

--- app/controllers/designer_controller.rb
+++ app/controllers/designer_controller.rb
@@ -1,7 +1,13 @@
 class DesignerController < ApplicationController
   def index
+    @consoles = Console.order('name ASC').all
   end
 
   def works
+  end
+
+  def about
+    @designer = Designer.find(params[:id])
+
   end
 end
