f80266740bf59ef6ad399778aaa2028e6f58d183
juliamalovi <juliamalovi@gmail.com>
2013-06-22 14:29:15 +0200

    modified designer about page , controller

--- app/controllers/designer_controller.rb
+++ app/controllers/designer_controller.rb
@@ -10,9 +10,22 @@ class DesignerController < ApplicationController
   def about
     @designer = Designer.find(params[:id])
     @designerImage = "/designer/"+ @designer.img_url
+    @event_designer_list = EventsDesigners.where("designer_id = ?", params[:id])
+    if @event_designer_list != nil
+      @events = Array.new
+      @event_designer_list.each do |event_designer|
+        @event = Event.find(event_designer.event_id)
+        @events.push @event
+      end
+    end
+
   end
 
   def full_string
     "#{name} #{lastname}"
+  end
+
+  def designer_params
+    params.require(:name,:surname,:img_url).permit(:description, :did,:designer_id)
   end
 end
