a4b26a0f1baea9dbab178f4f94d73ce9d26a8e81
lucalant <luca@lant.it>
2013-06-23 21:21:29 +0200

    top_client html ed inizio controller

--- app/controllers/top_clients_controller.rb
+++ app/controllers/top_clients_controller.rb
@@ -1,7 +1,25 @@
 class TopClientsController < ApplicationController
   def show
+
+    @top_client = Top_client.find(params[:id])
+
+    @name =  params[:name]
+    @surname = params[:surname]
+    @img = params[:img_url]
+    @about = params[:about]
+    @function = params[:function]
+    @factory = params[:factory]
+
+
   end
 
   def all_top_clients
+
+
+
+
   end
+end
+class Top_client
+
 end
